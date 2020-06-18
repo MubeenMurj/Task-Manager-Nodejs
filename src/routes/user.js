b1f0af757e96493a58d45f7dfd0f2c06d647ecda
MubeenMurj <mrjazib1@gmail.com>
2020-06-19 04:24:11 +0500

    working on uploads

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const User = require("../models/user")
 const auth = require("../middleware/auth")
+const multer = require("multer")
 
 const router = new express.Router()
 
@@ -85,6 +86,14 @@ router.post("/users/logoutAll", auth, async (req, res) => {
 	} catch (e) {
 		res.status(500).send()
 	}
+})
+
+const upload = multer({
+	dest: "avatars",
+})
+
+router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
+	res.send()
 })
 
 module.exports = router
